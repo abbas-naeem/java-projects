14864276ae1be3cd45b612ccf615e5f19272bdfb
abbas-naeem <me.abbas.naeem@gmail.com>
2023-12-05 19:51:14 +0000

    java-module-2

---
+++ java-module-2/src/main/java/com/example/javamodule2/Decimals.java
@@ -0,0 +1,17 @@
+package com.example.javamodule2;
+
+public class Decimals {
+    public static void main(String[] args) {
+        double percentage = 55.7;
+        System.out.println("In 2020, " + percentage + "% of the world's population is urban");
+
+        double dividend = 25;
+        double divisor = 2;
+        System.out.println(dividend/divisor);
+
+    //        int dividend = 25;
+    //        int divisor = 2;
+    //        System.out.println(dividend/divisor);
+    //        My output would be 12 due to us using int, so avoid using int/long as much as possible during calculations
+    }
+}
