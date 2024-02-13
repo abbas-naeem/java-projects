a134b72766638294b789a7624e48fb959bd9b734
abbas-naeem <me.abbas.naeem@gmail.com>
2024-02-13 19:27:38 +0000

    java-module-2

---
+++ java-module-4/src/Parameters.java
@@ -0,0 +1,9 @@
+public class Parameters {
+    public static void main(String[] args) {
+        calculateArea(2.5, 3.5);
+    }
+
+    public static void calculateArea(double length, double width) {
+        System.out.println("Area: " + length * width);
+    }
+}
---
+++ java-module-4/src/Workbook41.java
@@ -0,0 +1,16 @@
+public class Workbook41 {
+    public static void main(String[] args) {
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+    }
+
+    public static void printLines() {
+        System.out.println("I will not copy and paste code");
+        System.out.println("I will use CamelCase for class names");
+        System.out.println("I will use lowerCamelCase for functions");
+        System.out.println("I will use lowerCamelCase for variables");
+    }
+}
