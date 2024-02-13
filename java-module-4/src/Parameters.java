64502b615ffb4dca3cc68dc83071c669f7dc991e
abbas-naeem <me.abbas.naeem@gmail.com>
2024-02-13 19:30:59 +0000

    java-module-2

--- java-module-4/src/Parameters.java
+++ java-module-4/src/Parameters.java
@@ -1,6 +1,8 @@
 public class Parameters {
     public static void main(String[] args) {
         calculateArea(2.5, 3.5);
+        calculateArea(2, 3);
+        calculateArea(10, 100);
     }
 
     public static void calculateArea(double length, double width) {
