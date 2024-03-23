61a7d478a172b4ca9b645fc7ec96111f552966ad
abbas-naeem <me.abbas.naeem@gmail.com>
2024-03-23 00:41:56 +0000

    java-module-2

--- java-module-4/src/Workbook43.java
+++ java-module-4/src/Workbook43.java
@@ -1,3 +1,10 @@
 public class Workbook43 {
+    public static void main(String[] args) {
+        conversionFromFahrenheit(77);
+    }
 
+    public static void conversionFromFahrenheit(double temperature) {
+        double finalAnswer = (temperature - 32) * 5 / 9;
+        System.out.println(finalAnswer);
+    }
 }
