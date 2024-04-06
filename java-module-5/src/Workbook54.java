efbc8d9a9bf0288563cf1a935fa3f55df8c36b7d
abbas-naeem <me.abbas.naeem@gmail.com>
2024-04-06 17:42:55 +0100

    java-module-2

---
+++ java-module-5/src/Workbook54.java
@@ -0,0 +1,10 @@
+public class Workbook54 {
+
+    public static void main(String[] args) {
+        for (int i = 99; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
+        }
+    }
+
+}
