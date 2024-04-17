f576d804cef176a6438049f66bcae68ad795af98
abbas-naeem <me.abbas.naeem@gmail.com>
2024-04-17 22:24:53 +0100

    java-module-2

--- java-module-5/src/WhileLoopExamples.java
+++ java-module-5/src/WhileLoopExamples.java
@@ -4,13 +4,29 @@ public class WhileLoopExamples {
         // Example 1: Roll a dice until a 6 is rolled
         // TODO: Write a while loop that simulates rolling a dice until a 6 is rolled
 
-
+        int diceRoll = -1;
+        while (diceRoll != 6) {
+            diceRoll = rollDice();
+        }
 
         // Example 2: Generate random numbers between 1 and 100 until a number greater than 90 is generated
         // TODO: Write a while loop that generates random numbers between 1 and 100 until a number greater than 90 is generated
 
+        int number = 0;
+        while (number < 90) {
+            number = generateRandomNumber();
+            System.out.println(number);
+        }
+
+
         // Example 3: Keep doubling a random number until it's greater than 1000
         // TODO: Write a while loop that keeps doubling the 'number' variable until it's greater than 1000
+
+        int ranNumb = generateRandomNumber();
+        while (ranNumb < 1000) {
+            ranNumb *= 2;
+            System.out.println(ranNumb);
+        }
     }
 
     // Function to simulate rolling a dice (returns a random integer between 1 and 6)
