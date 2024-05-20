700f42ba408d38a5052a85a8d6ce824ed1a2cc19
abbas-naeem <me.abbas.naeem@gmail.com>
2024-05-20 20:57:08 +0100

    java-module-2

---
+++ firstproject/src/concatination.java
@@ -0,0 +1,12 @@
+public class concatination {
+
+    public static void main(String[] args) {
+        String foreName = "Muhammad";
+        String middleName = "Abbas";
+        String surName = "Naeem";
+        String fullName = foreName + " " + middleName + " " + surName;
+        System.out.println(fullName);
+    }
+
+}
+
