b7683a2f8239a917670ac653d9cf36859234bb22
abbas-naeem <me.abbas.naeem@gmail.com>
2024-07-30 21:01:10 +0100

    java-module-2

---
+++ outofcourseproj/src/arays.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class arays {
+    public static void main(String[] args) {
+        double[] temp = {1.1, 2.2, 3.3, 4.4, 5.5};
+
+        String[] languages = {"Java", "Python", "C++"};
+
+        System.out.println(Arrays.toString(temp));
+        System.out.println(Arrays.toString(languages));
+
+
+
+    }
+}
---
+++ outofcourseproj/src/loopArray.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class loopArray {
+    public static void main(String[] args) {
+        int[] ages = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+        }
+    }
+}
