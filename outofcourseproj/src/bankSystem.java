a27b5a98317fbe3ee05878fd71063cb936bd444f
abbas-naeem <me.abbas.naeem@gmail.com>
2024-08-07 21:02:46 +0100

    java-module-2

--- outofcourseproj/src/bankSystem.java
+++ outofcourseproj/src/bankSystem.java
@@ -1,3 +1,70 @@
-public class bankSystem {
+import java.util.Scanner;
 
+public class bankSystem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to Java Bank LTD");
+
+        System.out.println("You have a free starter pack money from Java Bank LTD of £3000");
+
+        bankSystem_Link current = new bankSystem_Link(678578, "Muhammad Abbas Naeem", 3000);
+
+        while (true) {
+
+            System.out.print("Would you like to make a transaction, deposit or withdrawal: ");
+            String option = scan.nextLine();
+
+            if (option.equals("transaction") || option.equals("Transaction")) {
+                System.out.println("Welcome to the Java Bank Transaction System!");
+
+                System.out.print("Please enter the account number: ");
+                int accountNumber = scan.nextInt();
+                scan.nextLine();
+
+                System.out.print("Please enter the account holder name: ");
+                String accountHolderName = scan.nextLine();
+
+                while (true) {
+                    System.out.print("Please enter the amount you want to send: ");
+                    int send = scan.nextInt();
+                    if (send > current.getBalance()) {
+                        System.out.println(" ");
+                        System.out.println("Your balance is too low!");
+                    } else {
+                        System.out.println(" ");
+                        System.out.println("Your payment to " + accountHolderName + " has successfully been sent!");
+                    }
+                }
+
+
+            } else if (option.equals("deposit") || option.equals("Deposit")) {
+                System.out.println("Welcome to the Java Bank Deposit System");
+
+                System.out.print("Please enter an amount you want to deposit into this account: ");
+                double amountIn = scan.nextInt();
+
+                current.setBalance(amountIn += current.getBalance());
+            } else if (option.equals("withdrawal") || option.equals("Withdrawal")) {
+                System.out.println("Welcome to the Java Bank Withdrawal System");
+
+                while (true) {
+                    System.out.print("Please enter an amount you want to withdraw from this account: ");
+                    double amountOut = scan.nextInt();
+
+                    if (amountOut > current.getBalance()) {
+                        System.out.println(" ");
+                        System.out.println("Your balance is too low");
+                    } else {
+                        current.setBalance(current.getBalance() - amountOut);
+                        System.out.println(" ");
+                        System.out.println("You have successfully withdrew " + amountOut);
+                        break;
+                    }
+
+                }
+
+            }
+        }
+    }
 }
--- outofcourseproj/src/bankSystem_Link.java
+++ outofcourseproj/src/bankSystem_Link.java
@@ -1,2 +1,40 @@
 public class bankSystem_Link {
+    private int accountNumber;
+
+    private String accountHolder;
+
+    private double balance;
+
+
+    public bankSystem_Link (int accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
+
