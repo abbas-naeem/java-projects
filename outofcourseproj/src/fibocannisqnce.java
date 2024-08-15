28c58fe4c0483601e6280a5aef3e81e9d8021fa0
abbas-naeem <me.abbas.naeem@gmail.com>
2024-08-15 21:24:04 +0100

    java-module-2

---
+++ outofcourseproj/src/fibocannisqnce.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class fibocannisqnce {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Fibonacci Sequence Java Utility");
+
+        System.out.print("Please print your first number: ");
+        int firstNumber = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("Please print your second number: ");
+        int secondNumber = scan.nextInt();
+
+        System.out.print("Please big do you want your sequence to be: ");
+        int sequenceSize = scan.nextInt();
+
+        int[] sequence = new int[sequenceSize];
+
+        sequence[0] = firstNumber;
+
+        sequence[1] = secondNumber;
+
+        for (int i = 2; i > sequenceSize; i++) {
+            int num = sequence[i] - sequence[i+1];
+            sequence[i] = num;
+        }
+
+        System.out.println(Arrays.toString(sequence));
+
+    }
+
+}
