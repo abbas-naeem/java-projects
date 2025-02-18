73e650db16e5f8c9e9e44c0662ef8204a3b8c1c1
abbas-naeem <123333635+abbas-naeem@users.noreply.github.com>
2025-02-18 21:26:44 +0000

    .

---
+++ java-module-1/src/updateVariables.java
@@ -0,0 +1,22 @@
+public class updateVariables {
+    public static void main(String[] args) {
+        // Integer
+        int passangers = 6;
+
+        // Complicated Method:
+        //        passangers = passangers + 8;
+        // Simple Method:
+        passangers += 8;
+
+        System.out.println(passangers);
+
+        // Updating Integer
+        int busTickets = passangers;
+        System.out.println(busTickets);
+
+        // The max value is 2147483647 (Not needed for general info)
+        int maxNumber = 2147483647;
+        System.out.println(maxNumber);
+
+    }
+}
